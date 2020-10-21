9501c9b48b0fd8d47a7e65b6c5b360a7d01d70db
FOZLAUR <fozla@oocl.com>
2020-10-21 16:59:42 +0800

    [spring-boot-employee][LaurenceFoz&ErikNobleta] refactor: moved logic from EmployeeController to EmployeeRepository

--- src/main/java/com/thoughtworks/springbootemployee/repositories/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repositories/EmployeeRepository.java
@@ -3,11 +3,14 @@ package com.thoughtworks.springbootemployee.repositories;
 import com.thoughtworks.springbootemployee.models.Employee;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class EmployeeRepository {
+    private final List<Employee> employeeList = new ArrayList<>();
+
     public List<Employee> findAll() {
-        return null;
+        return employeeList;
     }
 }
