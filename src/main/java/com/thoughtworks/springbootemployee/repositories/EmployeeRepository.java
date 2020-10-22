ba00ebcaa5b929188b074e74cac61da79ef17abc
FOZLAUR <fozla@oocl.com>
2020-10-22 14:53:05 +0800

    [spring-boot-employee][Laurence&Andrey] feature: Added logic for the EmployeeService

--- src/main/java/com/thoughtworks/springbootemployee/repositories/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repositories/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.thoughtworks.springbootemployee.repositories;
 import com.thoughtworks.springbootemployee.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByGender(String gender);
 }
--- src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
@@ -1,45 +1,56 @@
 package com.thoughtworks.springbootemployee.services;
 
 import com.thoughtworks.springbootemployee.models.Employee;
-import com.thoughtworks.springbootemployee.repositories.EmployeeRepositoryLegacy;
+import com.thoughtworks.springbootemployee.repositories.EmployeeRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    private final EmployeeRepositoryLegacy employeeRepositoryLegacy;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeService(EmployeeRepositoryLegacy employeeRepositoryLegacy) {
-        this.employeeRepositoryLegacy = employeeRepositoryLegacy;
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     public List<Employee> getAll() {
-        return employeeRepositoryLegacy.findAll();
+        return employeeRepository.findAll();
     }
 
     public Employee createEmployee(Employee employee) {
-        employeeRepositoryLegacy.addEmployee(employee);
+        employeeRepository.save(employee);
         return employee;
     }
 
-    public void updateEmployee(int employeeID, Employee updatedEmployee) {
-        employeeRepositoryLegacy.updateEmployee(employeeID, updatedEmployee);
+    public Employee updateEmployee(int employeeID, Employee updatedEmployee) {
+        Employee employee = findEmployee(employeeID);
+        employee.setEmployeeId(updatedEmployee.getEmployeeId());
+        employee.setName(updatedEmployee.getName());
+        employee.setAge(updatedEmployee.getAge());
+        employee.setGender(updatedEmployee.getGender());
+        employee.setSalary(updatedEmployee.getSalary());
+        employee.setCompanyId(updatedEmployee.getCompanyId());
+        employeeRepository.save(employee);
+        return employee;
     }
 
     public void deleteEmployee(int employeeID) {
-        employeeRepositoryLegacy.deleteEmployee(employeeID);
+        employeeRepository.deleteById(employeeID);
     }
 
     public Employee findEmployee(int employeeID) {
-        return employeeRepositoryLegacy.findEmployeeByID(employeeID);
+        return employeeRepository.findById(employeeID).orElse(null);
     }
 
     public List<Employee> findEmployeesByGender(String gender) {
-        return employeeRepositoryLegacy.findEmployeesByGender(gender);
+        return employeeRepository.findByGender(gender);
     }
 
     public List<Employee> pagination(int page, int pageSize) {
-        return employeeRepositoryLegacy.pagination(page, pageSize);
+        Pageable pageable = PageRequest.of(page,pageSize);
+        return employeeRepository.findAll(pageable).toList();
     }
 }
